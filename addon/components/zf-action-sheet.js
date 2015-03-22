ccaa3fda74e2b52c5a76903bba1b7c3a8d08a3b7
A. Tate Barber <tate@barber.so>
2015-03-22 11:49:45 -0500

    Added action-sheet component.

---
+++ addon/components/zf-action-sheet.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import ZfAnimationMixin from '../mixins/zf-animation';
+
+var ZfActionSheetComponent = Ember.Component.extend( ZfAnimationMixin, {
+  classNames: ['action-sheet'],
+  classNameBindings: ['position'],
+  position: 'bottom',
+
+  initClassses: ['is-active-add', 'is-active-remove'],
+  transitionClassses: ['is-active-add-active', 'is-active-remove-active'],
+
+  animationSpeed: 'slow',
+
+  animationIn: (function() {
+    var className;
+    switch (this.get('position')) {
+      case 'top':
+        className = 'slideInDown';
+      break;
+
+      case 'bottom':
+        className = 'slideInUp';
+      break;
+
+      case 'left':
+        className = 'slideInLeft';
+      break;
+
+      case 'right':
+        className = 'slideInRight';
+      break;
+    }
+    return className;
+  }).property('position'),
+
+  animationOut: (function() {
+    var className;
+    switch (this.get('position')) {
+      case 'top':
+        className = 'slideOutUp';
+      break;
+
+      case 'bottom':
+        className = 'slideOutDown';
+      break;
+
+      case 'left':
+        className = 'slideOutRight';
+      break;
+
+      case 'right':
+        className = 'slideOutLeft';
+      break;
+    }
+    return className;
+  }).property('position'),
+});
+
+export default ZfActionSheetComponent;
